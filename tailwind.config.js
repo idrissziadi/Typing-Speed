615a5e07c05ca4a82e4c33ba09a60fb1996b56b3
idrissziadi <idriss.ziadi47@gmail.com>
2024-09-07 13:39:12 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Ajoutez le chemin à vos fichiers source React
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
